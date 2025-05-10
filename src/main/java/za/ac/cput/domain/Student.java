5be908b0985240132097d099382711b89f332d22
Salaamah Peck <230207170@mycput.ac.za>
2025-05-10 16:44:47 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/domain/Student.java
@@ -0,0 +1,81 @@
+/*Student.java
+Student POJO class
+Author: S Peck (230207170)
+Date: 10/05/2025
+ */
+package za.ac.cput.domain;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+//@Entity
+//@DiscriminatorValue("Student")
+public class Student extends User{
+   private String studentNumber;
+
+    public Student() {
+    }
+
+    private Student (Builder builder) {
+        super(builder.password, builder.name, builder.surname, builder.phone, builder.email);
+        this.studentNumber = builder.studentNumber;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentNumber='" + studentNumber + '\'' +
+                ", id=" + id +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String studentNumber;
+        private String password;
+        private String name;
+        private String surname;
+        private String phone;
+        private String email;
+
+        public Builder setStudentNumber(String studentNumber){
+            this.studentNumber = studentNumber;
+            return this;
+
+        }
+        public Builder setPassword(String password){
+            this.password = password;
+            return this;
+        }
+        public Builder setName(String name){
+            this.name = name;
+            return this;
+        }
+        public Builder setSurname(String surname){
+            this.surname = surname;
+            return this;
+        }
+        public Builder setPhone(String phone){
+            this.phone = phone;
+            return this;
+        }
+        public Builder setEmail(String email){
+            this.email = email;
+            return this;
+        }
+        public Student build() {
+            return new Student(this);
+        }
+    }
+
+
+}
+
---
+++ src/main/java/za/ac/cput/domain/User.java
@@ -0,0 +1,93 @@
+/*User.java
+User POJO class
+Author: S Peck (230207170)
+Date: 10/05/2025
+ */
+package za.ac.cput.domain;
+
+import jakarta.persistence.*;
+
+//@Entity
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@DiscriminatorColumn(name = "User", discriminatorType = DiscriminatorType.STRING)
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+    protected String password;
+    protected String name;
+    protected String surname;
+    protected String phone;
+    protected String email;
+
+    public User() {
+    }
+
+    public User(String password, String name, String surname, String phone, String email) {
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
+
+
+
