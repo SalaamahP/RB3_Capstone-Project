2f4206bf0f9cb4e96e76c6b3270f568522c7a2e9
Salaamah Peck <230207170@mycput.ac.za>
2025-05-18 12:07:12 +0200

    pom file update

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -3,9 +3,9 @@ package za.ac.cput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        //SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
         }
     }
