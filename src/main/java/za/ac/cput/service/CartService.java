0476fee37bc4ae0cfb71e4e8e789ba9b4a5e884e
Nobahle Vuyiswa Nzimande <nobahlenzimande85@gmail.com>
2025-08-31 16:47:17 +0200

    Update CartService.java

--- src/main/java/za/ac/cput/service/CartService.java
+++ src/main/java/za/ac/cput/service/CartService.java
@@ -3,13 +3,14 @@
  * Author: Nobahle Vuyiswa Nzimande (222641533)
  * Date: 25 May 2025
  */
-
 package za.ac.cput.service;
 
 import za.ac.cput.domain.Cart;
 import java.util.List;
 
 public interface CartService extends IService<Cart, Long> {
+    @Override
+    Cart read(Long id); 
+    
     List<Cart> getAll();
 }
-
