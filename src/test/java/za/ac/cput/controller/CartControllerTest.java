1c8ff3960d3547010d574f9e5f4cb6871466cd0c
Nobahle Vuyiswa Nzimande <nobahlenzimande85@gmail.com>
2025-08-31 16:55:03 +0200

    Update CartControllerTest.java

--- src/test/java/za/ac/cput/controller/CartControllerTest.java
+++ src/test/java/za/ac/cput/controller/CartControllerTest.java
@@ -1,9 +1,3 @@
-/*
- * CartControllerTest.java
- * Integration test class for CartController
- * Author: Nobahle Vuyiswa Nzimande (222641533)
- * Date: 21 August 2025
- */
 package za.ac.cput.controller;
 
 import org.junit.jupiter.api.*;
@@ -11,11 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import za.ac.cput.domain.Cart;
 import za.ac.cput.domain.Cart.PaymentOption;
-import za.ac.cput.factory.CartFactory;
 
 import java.time.LocalDateTime;
 
@@ -23,81 +15,75 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class CartControllerTest {
-
-    @LocalServerPort
-    private int port;
+class CartControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
 
     private String BASE_URL;
 
     private static Cart cart;
 
-    @BeforeAll
-    public static void setUp() {
-        // Create a Cart object using factory
-        cart = CartFactory.createCart(
-                "user123",
-                PaymentOption.CASH,
-                LocalDateTime.now()
-        );
-    }
-
     @BeforeEach
     void init() {
-        BASE_URL = "http://localhost:" + port + "/api/cart";
+        BASE_URL = "http://localhost:" + port + "/SEMS/cart";
+
+    }
+
+    @BeforeAll
+    static void setUp() {
+        cart = new Cart.Builder()
+                .setUserId("user123")
+                .setPaymentOption(PaymentOption.CASH)
+                .setBookingDate(LocalDateTime.now())
+                .build();
     }
 
     @Test
     @Order(1)
     void create() {
-        ResponseEntity<Cart> response = restTemplate.postForEntity(BASE_URL, cart, Cart.class);
-        assertNotNull(response.getBody(), "Created cart should not be null");
-
-        cart = response.getBody(); // Save the generated cartId for future tests
-        assertEquals("user123", cart.getUserId(), "User ID should match the factory input");
-        System.out.println("Created Cart: " + cart);
+        ResponseEntity<Cart> response = restTemplate.postForEntity(BASE_URL + "/create", cart, Cart.class);
+        assertNotNull(response.getBody());
+        cart = response.getBody();
+        assertTrue(cart.getCartId() > 0);
+        System.out.println("Created Cart ID: " + cart.getCartId());
     }
 
     @Test
     @Order(2)
     void read() {
-        ResponseEntity<Cart> response = restTemplate.getForEntity(BASE_URL + "/" + cart.getCartId(), Cart.class);
-        assertNotNull(response.getBody(), "Read cart should not be null");
-        assertEquals(cart.getCartId(), response.getBody().getCartId(), "Cart ID should match");
-        System.out.println("Read Cart: " + response.getBody());
+        ResponseEntity<Cart> response = restTemplate.getForEntity(BASE_URL + "/read/" + cart.getCartId(), Cart.class);
+        assertNotNull(response.getBody());
+        assertEquals(cart.getCartId(), response.getBody().getCartId());
+        System.out.println("Read Cart ID: " + response.getBody().getCartId());
     }
 
     @Test
     @Order(3)
     void update() {
-        // Update Cart details
         Cart updatedCart = new Cart.Builder()
-                .setCartId(cart.getCartId())
-                .setUserId("user456")
-                .setPaymentOption(PaymentOption.DEPOSIT)
-                .setBookingDate(cart.getBookingDate())
+                .copy(cart)
+                .setPaymentOption(PaymentOption.CASH)
                 .build();
 
-        restTemplate.put(BASE_URL, updatedCart);
+        restTemplate.put(BASE_URL + "/update", updatedCart);
 
-        ResponseEntity<Cart> response = restTemplate.getForEntity(BASE_URL + "/" + cart.getCartId(), Cart.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode(), "Response status should be OK");
-        assertNotNull(response.getBody(), "Updated cart should not be null");
-        assertEquals("user456", response.getBody().getUserId(), "User ID should be updated");
-
-        cart = response.getBody(); // Save updated cart
-        System.out.println("Updated Cart: " + cart);
+        ResponseEntity<Cart> response = restTemplate.getForEntity(BASE_URL + "/read/" + cart.getCartId(), Cart.class);
+        assertNotNull(response.getBody());
+        assertEquals(PaymentOption.CASH, response.getBody().getPaymentOption());
+        System.out.println("Updated Cart ID: " + response.getBody().getCartId());
+        cart = response.getBody();
     }
 
     @Test
     @Order(4)
     void getAll() {
-        ResponseEntity<Cart[]> response = restTemplate.getForEntity(BASE_URL, Cart[].class);
-        assertNotNull(response.getBody(), "GetAll should not return null");
-        assertTrue(response.getBody().length > 0, "There should be at least one cart");
+        ResponseEntity<Cart[]> response = restTemplate.getForEntity(BASE_URL + "/getAll", Cart[].class);
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody().length > 0);
         System.out.println("All Carts:");
         for (Cart c : response.getBody()) {
             System.out.println(c);
@@ -107,10 +93,10 @@ public class CartControllerTest {
     @Test
     @Order(5)
     void delete() {
-        restTemplate.delete(BASE_URL + "/" + cart.getCartId());
+        restTemplate.delete(BASE_URL + "/delete/" + cart.getCartId());
 
-        ResponseEntity<Cart> response = restTemplate.getForEntity(BASE_URL + "/" + cart.getCartId(), Cart.class);
-        assertNull(response.getBody(), "Deleted cart should be null");
+        ResponseEntity<Cart> response = restTemplate.getForEntity(BASE_URL + "/read/" + cart.getCartId(), Cart.class);
+        assertNull(response.getBody());
         System.out.println("Deleted Cart ID: " + cart.getCartId());
     }
 }
