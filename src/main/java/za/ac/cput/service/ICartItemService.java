46ef7252e96136e58b5f363b14fc74089518a194
Patience Phakathi <22228431@mycput.ac.za>
2025-10-19 01:12:38 +0200

    edited

---
+++ src/main/java/za/ac/cput/service/ICartItemService.java
@@ -0,0 +1,14 @@
+/* CarItemService.java
+ * CartItem service interface
+ * Author: Your Name (Your Student Number)
+ * Date: 26 August 2025
+ */
+package za.ac.cput.service;
+
+import za.ac.cput.domain.CartItem;
+
+import java.util.List;
+
+public interface ICartItemService extends IService<CartItem, Long> {
+    List<CartItem> getAll();
+}
